adc4ea1b4b6b4e5e824d23c253ec2c12957672c5
Lyndon Loh <108809431+lohputer@users.noreply.github.com>
2023-11-03 18:11:31 +0800

    cool

--- script.js
+++ script.js
@@ -2,9 +2,13 @@ var images = ["redstone.webp", "amethyst.webp"];
 var randoms = ["^`$+", "⟴:]*]^*]", "⟴;;&<", "@=`!", "!;$%"];
 function swap() {
     let choice = Math.floor(Math.random()*2.1);
+    let secondchoice = Math.floor(Math.random()*5);
     document.getElementById("img").src = "https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/HD_transparent_picture.png/1024px-HD_transparent_picture.png";
     setTimeout(()=>{
         document.getElementById("img").src = images[choice];
+        if (secondchoice === 4 && choice !== 2) {
+            document.getElementById("small").style.display = "block";
+        }
         if (choice === 1) {
             document.body.style.backgroundColor = "#a25afa";
         } else if (choice === 0) {
@@ -25,6 +29,7 @@ function swap() {
             }, 1000)
             setTimeout(()=>{
                 document.getElementsByTagName("h1")[0].innerHTML = "Redstone or Amethyst";
+                document.getElementsByTagName("button")[0].innerHTML = "Click me :)";
                 document.getElementsByTagName("h1")[0].style.transition = "1s linear";
                 document.getElementsByTagName("img")[0].style.transform = "";
                 document.getElementsByTagName("img")[0].style.transition = "1s linear";   
@@ -33,4 +38,18 @@ function swap() {
             }, 5000);
         }
     }, 10);
+}
+function message() {
+    document.getElementById("small").style.display = "none";
+    document.getElementsByTagName("img")[0].style.display = "none";
+    document.getElementsByTagName("button")[0].style.display = "none";
+    let timer = 100+Math.random()*2500;
+    setTimeout(()=>{
+        document.getElementsByTagName("h1")[0].innerHTML = "&~$@";
+    }, timer);
+    setTimeout(()=>{
+        document.getElementsByTagName("h1")[0].innerHTML = "Amethyst or Redstone";
+        document.getElementsByTagName("img")[0].style.display = "block";
+        document.getElementsByTagName("button")[0].style.display = "block";
+    }, timer+500);
 }
